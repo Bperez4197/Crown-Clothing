89cdf14069b350e04faf5b92e4b714c92d940b22
Bryce Perez <bryceperez31@gmail.com>
2022-01-04 12:59:29 -0700

    figuring out hosting

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import CollectionItem from "../collection-item/collection-item.component";
 
 import "./preview-collection.styles.scss";
 
-const PreviewCollection = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1
+      className="title"
+      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
     <div className="preview">
       {items
-        .filter((item, index) => index < 4)
+        .filter((item, idx) => idx < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
@@ -16,4 +23,4 @@ const PreviewCollection = ({ title, items }) => (
   </div>
 );
 
-export default PreviewCollection;
+export default withRouter(CollectionPreview);
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -31,31 +31,36 @@ const SHOP_DATA = {
       {
         id: 5,
         name: "Green Beanie",
-        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdnb.lystit.com%2Fphotos%2F532b-2014%2F12%2F16%2Ffree-people-green-capsule-slouchy-beanie-product-1-26477478-0-863980087-normal.jpeg&f=1&nofb=1",
         price: 18,
       },
       {
         id: 6,
         name: "Palm Tree Cap",
-        imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F43%2Fd5%2F0b%2F43d50b7e3347c2f10b11fca81ad9f8a1.jpg&f=1&nofb=1",
         price: 14,
       },
       {
         id: 7,
         name: "Red Beanie",
-        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F904947%2Fpexels-photo-904947.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26h%3D750%26w%3D1260&f=1&nofb=1",
         price: 18,
       },
       {
         id: 8,
         name: "Wolf Cap",
-        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.glacierwear.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fthumbnail%2F960x%2F17f82f742ffe127f42dca9de82fb58b1%2Fi%2Fm%2Fimage002_3_16.jpg&f=1&nofb=1",
         price: 14,
       },
       {
         id: 9,
         name: "Blue Snapback",
-        imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.thetrendspotter.net%2Fwp-content%2Fuploads%2F2017%2F02%2F7-Ways-to-Wear-a-Snapback.jpg&f=1&nofb=1",
         price: 16,
       },
     ],
@@ -92,25 +97,29 @@ const SHOP_DATA = {
       {
         id: 14,
         name: "Nike Red High Tops",
-        imageUrl: "https://i.ibb.co/QcvzydB/nikes-red.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feldiariony.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2018%2F08%2Fpaul-volkmer-522844-unsplash.jpg%3Fquality%3D80%26strip%3Dall&f=1&nofb=1",
         price: 160,
       },
       {
         id: 15,
         name: "Nike Brown High Tops",
-        imageUrl: "https://i.ibb.co/fMTV342/nike-brown.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsneakerbardetroit.com%2Fwp-content%2Fuploads%2F2016%2F01%2Fkevin-hart-nike-air-force-1-rt-mid-beige.jpg&f=1&nofb=1",
         price: 160,
       },
       {
         id: 16,
         name: "Air Jordan Limited",
-        imageUrl: "https://i.ibb.co/w4k6Ws9/nike-funky.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1102777%2Fpexels-photo-1102777.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-arthur-ogleznev-1102777.jpg%26fm%3Djpg&f=1&nofb=1",
         price: 190,
       },
       {
         id: 17,
         name: "Timberlands",
-        imageUrl: "https://i.ibb.co/Mhh6wBg/timberlands.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1520809382381-d95e6364bbdb%3Fixlib%3Drb-1.2.1%26auto%3Dformat%26fit%3Dcrop%26w%3D1000%26q%3D80&f=1&nofb=1",
         price: 200,
       },
     ],
@@ -147,7 +156,8 @@ const SHOP_DATA = {
       {
         id: 22,
         name: "Tan Trench",
-        imageUrl: "https://i.ibb.co/M6hHc3F/brown-trench.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.lookastic.com%2Flooks%2Ftrenchcoat-suit-dress-shirt-original-6759.jpg&f=1&nofb=1",
         price: 185,
       },
     ],
@@ -184,19 +194,22 @@ const SHOP_DATA = {
       {
         id: 27,
         name: "Striped Sweater",
-        imageUrl: "https://i.ibb.co/KmSkMbH/striped-sweater.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F80%2F40%2F60%2F80406086bd75e5fe4a06293b3d9e84cd.jpg&f=1&nofb=1",
         price: 45,
       },
       {
         id: 28,
         name: "Yellow Track Suit",
-        imageUrl: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1xKUHxlmWBuNkSndVq6AsApXak%2F2-Piece-Set-for-Women-Yellow-Tracksuit-Women-Hoodies-Pullover-Sweat-Suits-Sportswear-Track-Suit-Autumn.jpg&f=1&nofb=1",
         price: 135,
       },
       {
         id: 29,
         name: "White Blouse",
-        imageUrl: "https://i.ibb.co/qBcrsJg/white-vest.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.lifehack.org%2Fwp-content%2Fuploads%2F2014%2F12%2FWhite-blouse.jpg&f=1&nofb=1",
         price: 20,
       },
     ],
@@ -233,13 +246,15 @@ const SHOP_DATA = {
       {
         id: 34,
         name: "Jean Long Sleeve",
-        imageUrl: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2Fa69e9b21-895c-48f3-8b3c-8b66ff45814d_1.6ef12104a80ed39e4e7e12614209db73.jpeg&f=1&nofb=1",
         price: 40,
       },
       {
         id: 35,
         name: "Burgundy T-shirt",
-        imageUrl: "https://i.ibb.co/mh3VM1f/polka-dot-shirt.png",
+        imageUrl:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.dickies.com%2Fdw%2Fimage%2Fv2%2FAAYI_PRD%2Fon%2Fdemandware.static%2F-%2FSites-master-catalog-dickies%2Fdefault%2Fdwf0e1291d%2Fimages%2Fmain%2FWL450_BY_FR.jpg%3Fsw%3D1563%26sh%3D2000%26sm%3Dfit&f=1&nofb=1",
         price: 25,
       },
     ],
